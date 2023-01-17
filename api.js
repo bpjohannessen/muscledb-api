00b6930769b86fd432047cd925a5977c99bec549
Bjørn-Petter Johannessen <bpjohannessen@users.noreply.github.com>
2023-01-17 20:46:12 +0100

    Updated api
    Updated api for cors fix

--- api.js
+++ api.js
@@ -6,47 +6,73 @@
  * 2023
  * 
  */
-const express = require("express");
-const http = require("http");
 
-/* Config for the http server*/
-const http_config = {
-    port: 3001,
-    host: "localhost",
-}
-  
-  const app = express();  
-  const http_server = http.createServer(app);
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000 || process.env.PORT;
+const musclesRouter = require("./routes/muscles");
 
-/*
- * Routes
- */
+app.use(cors({
+  origin: '*'
+}));
 
-const musclesRouter = require("./routes/muscles.js");
-
-
-
-app.use(express.json());
-app.use(express.static('public'));
-
-/* Default route + ping route */
-
-app.get("/", (req, res) => {
-    res.status(200).json({message: "alive"});
-  });
-
-app.get("/ping", (req, res) => {
-    res.status(200).json({ping: "pong"});
+app.get('/', (req, res) => {
+  res.json({ping: "pong2"});
 });
 
-/* Start the server */
-http_server.listen(http_config.port, http_config.host, () => {
-  console.log(`Listening on ${http_config.host}:${http_config.port}`);
-});
 
-/*
- * get routes for muscles
- */
-
-/* Get all muscles */
 app.use("/muscles", musclesRouter);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port} with cors 2`);
+});
+// Old code
+
+// /*
+//  *
+//  * api.js
+//  * Starts the restful js api for muscledb
+//  * Bjørn-Petter Johannessen
+//  * 2023
+//  * 
+//  */
+// const express = require("express");
+// const http = require("http");
+// const musclesRouter = require("./routes/muscles");
+
+// /* Config for the http server*/
+// const http_config = {
+//     port: 3000,
+//     host: "localhost",
+// }
+  
+//   const app = express();  
+//   //const http_server = http.createServer(app);
+//   http.createServer((req, res) => {
+//     const headers = {
+//       'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+//       'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+//       'Access-Control-Max-Age': 2592000, // 30 days
+//       /** add other headers as per requirement */
+//     };
+  
+//     if (req.method === 'OPTIONS') {
+//       res.writeHead(204, headers);
+//       res.end();
+//       return;
+//     }
+  
+//     if (['GET', 'POST'].indexOf(req.method) > -1) {
+//       res.writeHead(200, headers);
+//       res.end('Hello World');
+//       return;
+//     }
+  
+//     res.writeHead(405, headers);
+//     res.end(`${req.method} is not allowed for the request.`);
+//   }).listen(http_config.port);
+
+// app.use("/muscles", musclesRouter);
--- routes/muscles.js
+++ routes/muscles.js
@@ -5,7 +5,10 @@ const muscles = require("../services/muscles");
 /* GET muscles listening */
 
 router.get("/", (req, res) => {
+    console.log("Fetching all muscles");
+
     return res.json(muscles.getAll());
+
 });
 
 module.exports = router;
